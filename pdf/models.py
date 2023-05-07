eac504f3e53b7fb31ac6fda7b2556b941037c2b4
“Gjorgi <georgiedelev@gmail.com>
2023-05-07 13:59:34 +0200

    Profile Models

--- pdf/models.py
+++ pdf/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    summary = models.CharField(max_length=255)
+    degree = models.CharField(max_length=255)
+    school = models.CharField(max_length=255)
+    university = models.CharField(max_length=255)
+    previous_work = models.CharField(max_length=255)
+    skills = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
